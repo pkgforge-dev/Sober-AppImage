676576f4b8dfe20948c2530cb027efd379648d51
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-07-18 21:16:41 -0400

    add `libadwaita`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm webkitgtk-6.0
+pacman -Syu --noconfirm webkitgtk-6.0 libadwaita
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
